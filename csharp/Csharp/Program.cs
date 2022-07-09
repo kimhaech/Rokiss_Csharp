8f233e1f55b05385484b4fe5f38f50263bfa8ceb
kimhaech <epiclog@hufs.ac.kr>
2022-07-10 02:35:58 +0900

    first commit 0710

---
+++ csharp/Csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp;
+
+            hp = 100;
+
+            Console.WriteLine("Hello Number ! {0}", hp);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
