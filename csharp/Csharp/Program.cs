ae21bef74fd231cf8061c6b1dbc779e20e0bb1a4
kimhaech <epiclog@hufs.ac.kr>
2022-07-14 21:01:44 +0900

    0714 자료형

--- csharp/Csharp/Program.cs
+++ csharp/Csharp/Program.cs
@@ -6,12 +6,32 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
+            // 정수
             int hp;
 
             hp = 100;
 
             Console.WriteLine("Hello Number ! {0}", hp);
-            //Console.WriteLine("Hello World!");
+
+            // boolean
+            bool check;
+            check = true;
+            check = false;
+
+            // 소수
+            float t = 3.14f;
+            double d;
+            d = 3.14;
+
+            // 문자 (2byte)
+            char c;
+
+            // 문자열
+            string s;
+            s = "I'm hungry...";
+
+            Console.WriteLine(s);
+
         }
     }
 }
