dafaffd758811ec9a47a301fecb954925ebe2f50
kimhaech <epiclog@hufs.ac.kr>
2022-07-15 19:12:42 +0900

    연산1

--- csharp/Csharp/Program.cs
+++ csharp/Csharp/Program.cs
@@ -6,39 +6,63 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
-            // 정수
-            int hp;
-            
-            hp = 100;
+            //// 정수
+            //int hp;
 
-            Console.WriteLine("Hello Number ! {0}", hp);
+            //hp = 0xFFFF;
+            //short bb = (short)hp;
 
-            // boolean
-            bool check;
-            check = true;
-            check = false;
+            //Console.WriteLine("Hello Number ! {0}", bb);
 
-            // 소수
-            float t = 3.14f;
-            double d;
-            d = 3.14;
+            //// boolean
+            //bool check;
+            //check = true;
+            //check = false;
 
-            // 문자 (2byte)
-            char c;
+            //// 소수
+            //float t = 3.14f;
+            //double d;
+            //d = 3.14;
+            //d = (double)t;
 
-            // 문자열
-            string s;
-            s = "I'm hungry...";
+            //// 문자 (2byte)
+            //char c;
 
-            Console.WriteLine(s);
+            //// 문자열
+            //string s;
+            //s = "I'm hungry...";
 
-            // 형변환
-            int a = 1000;
-            short b = (short)a;
+            //Console.WriteLine(s);
 
-            byte bt = 255;
-            sbyte sb = (sbyte)bt;
-            // 0xFF = 0b11111111 = -1
+            //// 형변환
+            //int a = 1000;
+            //short b = (short)a;
+
+            //byte bt = 255;
+            //sbyte sb = (sbyte)bt;
+            //// 0xFF = 0b11111111 = -1
+
+            int hp = 100;
+            int level = 50;
+            hp = 100 + 2 * 3 - (15/3)%2;
+
+            Console.WriteLine(hp);
+
+            bool b = (hp > 0);
+
+            Console.WriteLine(b);
+
+            bool isAlive = (hp > 0);
+            bool isHighLevel = (level >= 40);
+
+            // a = 살아있는 고렙 유저인가요?
+            bool a = isAlive && isHighLevel;
+
+            // b = 살아있거나, 고렙 유저이거나, 둘 중 하나인가?
+            b = isAlive || isHighLevel;
+
+            // c = 죽은 유저인가?
+            bool c = !isAlive;
 
 
         }
