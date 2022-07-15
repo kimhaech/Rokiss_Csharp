e94d673c1f8d404e625d871498e6f8f89aee24d7
kimhaech <epiclog@hufs.ac.kr>
2022-07-15 20:33:57 +0900

    loop

--- csharp/Csharp/Program.cs
+++ csharp/Csharp/Program.cs
@@ -4,68 +4,35 @@ namespace Csharp
 {
     class Program
     {
-        enum Choice
-        {
-            Rock = 1,
-            Paper = 2,
-            Scissors = 0
-        }
-
         static void Main(string[] args)
         {
-            // 0 : 가위 1 : 바위 2 : 보
+            // while 반복문
+            //int count = 5;
+
+            //while (count > 0)
+            //{
+            //    Console.WriteLine("Hello world");
+            //    count--;
+            //}
+
+            // do while
+            //string answer;
+            //do
+            //{
+            //    Console.WriteLine("you happy?");
+            //    answer = Console.ReadLine();
+            //} while (answer != "y");
+
+            // for 반복문
             
-            Random random = new Random();
-            int aiChoice = random.Next(0, 3); // 0~2사이의 랜덤 값
-
-            // 사용자에게 입력 받기
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            // 사용자의 값
-            switch (choice)
+            for(int i = 1; i <= 100; i++)
             {
-                case (int)Choice.Scissors:
-                    Console.WriteLine("당신의 선택은 가위입니다.");
-                    break;
-                case (int)Choice.Rock:
-                    Console.WriteLine("당신의 선택은 바위입니다.");
-                    break;
-                case (int)Choice.Paper:
-                    Console.WriteLine("당신의 선택은 보입니다.");
-                    break;
-            }
-            // ai의 값
-            switch (aiChoice)
-            {
-                case (int)Choice.Scissors:
-                    Console.WriteLine("ai의 선택은 가위입니다.");
-                    break;
-                case (int)Choice.Rock:
-                    Console.WriteLine("ai의 선택은 바위입니다.");
-                    break;
-                case (int)Choice.Paper:
-                    Console.WriteLine("ai의 선택은 보입니다.");
-                    break;
+                if ((i % 3) != 0)
+                    continue;
+                Console.WriteLine($"3으로 나뉘는 숫자 발견 : {i}");
             }
 
-            // 승부 결정
-
-            if (choice == aiChoice)
-            {
-                Console.WriteLine("무승부 입니다.");
-            }
-            else
-            {
-                if ((choice == (int)Choice.Rock && aiChoice == (int)Choice.Scissors) || (choice == (int)Choice.Scissors && aiChoice == (int)Choice.Paper) || (choice == (int)Choice.Paper && aiChoice == (int)Choice.Rock))
-                {
-                    Console.WriteLine("승리입니다,");
-                }
-                else
-                {
-                    Console.WriteLine("패배입니다.");
-                }
-                
-            }
+            
         }
     }
 }
