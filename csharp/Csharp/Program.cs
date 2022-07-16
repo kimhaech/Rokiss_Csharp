e92e44ff1ae9c1e9be1e1e7fb95caab74b972b5e
kimhaech <epiclog@hufs.ac.kr>
2022-07-16 19:12:15 +0900

    연습문제

--- csharp/Csharp/Program.cs
+++ csharp/Csharp/Program.cs
@@ -4,35 +4,36 @@ namespace Csharp
 {
     class Program
     {
+        static int Factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            return n*Factorial(n - 1);
+        }
         static void Main(string[] args)
         {
-            // while 반복문
-            //int count = 5;
-
-            //while (count > 0)
-            //{
-            //    Console.WriteLine("Hello world");
-            //    count--;
-            //}
-
-            // do while
-            //string answer;
-            //do
-            //{
-            //    Console.WriteLine("you happy?");
-            //    answer = Console.ReadLine();
-            //} while (answer != "y");
-
-            // for 반복문
-            
-            for(int i = 1; i <= 100; i++)
+            Console.WriteLine("구구단");
+            for(int i = 1; i < 10; i++)
             {
-                if ((i % 3) != 0)
-                    continue;
-                Console.WriteLine($"3으로 나뉘는 숫자 발견 : {i}");
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine(i + " X " + j + " = " + i*j);
+                }
             }
 
-            
+            Console.WriteLine("\n별출력");
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n팩토리얼");
+            int ret = Factorial(5);
+            Console.WriteLine(ret);
         }
     }
 }
