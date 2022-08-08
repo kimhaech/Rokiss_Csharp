0c317baf4d470de6e652bc6c6c050ba7e7049397
haechi <42764085+kimhaech@users.noreply.github.com>
2022-08-09 02:26:31 +0900

    Create Program.cs

---
+++ csharp/Csharp/textRPG/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Csharp
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            while (true)
+            {
+                game.Process();
+            }
+        }
+    }
+}
