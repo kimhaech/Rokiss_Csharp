0ca01741adb47842e45766ed80ae251ec5e0208f
haechi <42764085+kimhaech@users.noreply.github.com>
2022-08-08 18:52:42 +0900

    상속성 공부

---
+++ csharp/Csharp/상속성.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace Csharp
+{
+    class Player // 부모, 기반
+    {
+        // 필드
+        static public int counter = 1;
+        public int id;
+        public int hp;
+        public int attack;
+        
+       public Player()
+       {
+            Console.WriteLine("Player 생성자 호출!");
+       }
+       
+        public Player(int hp)
+        {
+            Console.WriteLine("Plyaer hp 생성자 호출!");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Player Move");
+        }
+    }
+
+    class Mage
+    {
+
+    }
+
+    class Archer
+    {
+
+    }
+
+    class Knight : Player // 자식, 파생
+    {
+        int c;
+
+        public Knight() : base(100)
+        {
+            this.c = 10;
+            base.hp = 100;
+
+            Console.WriteLine("Knight 생성자 호출!");
+        }
+        
+        static public Knight CreateKinght()
+        {
+            Knight knight = new Knight();
+            knight.hp = 100;
+            knight.attack = 10;
+
+            return knight;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = Knight.CreateKinght();
+            knight.Move();
+
+        }
+    }
+}
